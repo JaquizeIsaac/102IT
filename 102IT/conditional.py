adda980e56dd6e1707d5cdfbb64e365b8fe6632c
Myyel Isaac <147030259+JaquizeIsaac@users.noreply.github.com>
2025-05-23 10:35:15 -0700

    Added send_message function for loop assignment

--- 102IT/conditional.py
+++ 102IT/conditional.py
@@ -1,9 +1,11 @@
+def send_message():
+    for _ in range(10):
+        print("Yeah it is")
+
 answer = input("Is today a good day? (y/n): ")
 
 if answer.lower() == "y":
-    for _ in range(10):
-        print("Yes it is")
-
+    send_message()
 
 
 from colorama import init, Fore
